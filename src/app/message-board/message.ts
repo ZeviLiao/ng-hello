a5f7b70deb25f9f5a877c5be10d5212f300e1ef2
Zevi Liao <jointjm@gmail.com>
2020-07-31 13:55:52 +0800

    update

--- src/app/message-board/message.ts
+++ src/app/message-board/message.ts
@@ -1,30 +1,10 @@
 export class Message {
-
   constructor(name: string, content: string) {
-
     this.name = name;
     this.content = content;
     this.date = new Date();
-
   }
-  /**
-   * 留言者的名稱
-   *
-   */
   name: string;
-
-  /**
-   * 留言的內容
-   *
-   */
   content: string;
-
-  /**
-   * 留言的日期
-   *
-   */
   date: Date;
-
-
-
 }
