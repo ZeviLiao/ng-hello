64aca526a1d67ca90d2c0c4128609e429abbf067
Zevi Liao <jointjm@gmail.com>
2020-07-31 14:58:14 +0800

    add lookfeel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { MessageBoardComponent } from './message-board/message-board.component';
 import { FormsModule } from '@angular/forms';
+import { TodoListModule } from './todo-list/todo-list.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    TodoListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
